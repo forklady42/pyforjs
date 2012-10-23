ff62bc498fe46ff6b815e4763ce0c60bb6cd74f9
Kenya <kenya.vs@gmail.com>
2012-10-23 12:56:55 -0400

    added parens functionality

--- parser.py
+++ parser.py
@@ -5,12 +5,11 @@
 }"""
 
 #tokens = [['number', 12], ['operator', '+'], ['number', 4], ['operator', '/'], ['number', 6]]
-tokens = [['operator', '-'],['number', 12],['operator', '/'], ['number', 6]]
-
-#tokens = [['number', 12], ['operator', '+'], ['number', 4]]
+tokens = [['number', '4'], ['operator', '*'],['operator', '('],['operator', '-'],['number', 12],['operator', '+'], ['number', 6]]
 
 counter = 0
 current_token = tokens[0]
+
 
 def nud(l=''): #this empty string argument was a work around b/c when current token is number. the function that should be called is nud, but the algorithm doesn't seem to consider condidition within while loop?
     left = {'type':current_token[0], 'value':current_token[1]}
@@ -36,8 +35,18 @@ def neg(right, t):
     tree = {'type':t[1], 'right': right}
     return tree
 
-op_symbols = {'+': {'lbp':10, 'led':add}, '/':{'lbp':20, 'led':div}, '*':{'lbp':20, 'led':mul}, '-':{'lbp':10, 'led':sub}, '-u':{'lbp':1, 'led':neg}}
+op_symbols = {'+': {'lbp':10, 'led':add}, '/':{'lbp':20, 'led':div}, '*':{'lbp':20, 'led':mul}, '-':{'lbp':10, 'led':sub}, 
+              '-u':{'lbp':1, 'led':neg}}
 symbols = {'number':{'lbp':1, 'nud': nud, 'led':nud } ,'operator':op_symbols, 'end':{'lbp':0}}
+
+def paren():
+    value = expression(0)
+
+    if current_token[1] != ")":
+        print "Expecting closing parenthesis"
+
+    return value
+
 
 def read_next_token():
     global counter, current_token
@@ -70,6 +79,12 @@ def expression(rbp=0):
             left = symbols[t[0]][t[1]]['led'](right, t)
 
             return traverse(left, rbp)
+        elif current_token[1] == "(":
+            t = current_token
+            read_next_token()
+            left = paren()
+            
+            return traverse(left, rbp)
 
         else:
             print "Invalid token. Number expected..."
@@ -82,7 +97,10 @@ def traverse(left, rbp):
     try:
         lbp = symbols[current_token[0]]['lbp']
     except KeyError:
-        lbp = symbols[current_token[0]][current_token[1]]['lbp']
+        if current_token[1] == ")":
+            return left
+        else:
+            lbp = symbols[current_token[0]][current_token[1]]['lbp']
 
     while rbp < lbp:
         t = current_token
@@ -92,7 +110,10 @@ def traverse(left, rbp):
         try:
             lbp = symbols[current_token[0]]['lbp']
         except KeyError:
-            lbp = symbols[current_token[0]][current_token[1]]['lbp']
+            if current_token[1] == ")":
+                return left
+            else:
+                lbp = symbols[current_token[0]][current_token[1]]['lbp']
 
     return left
 
